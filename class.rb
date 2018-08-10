79f04db0a16df36ac3d0f5cb5aebe26fcd599bd8
Leyla Alekperova <leslairy@gmail.com>
2018-08-10 18:01:17 +0000

    automatically backed up by learn

--- class.rb
+++ class.rb
@@ -11,7 +11,7 @@ b= New_York.new
 puts New_York.go_to_a_borough   #for my class
 #when you want to call this one :name of class(New_York) name of method(go_to_a_borough)
 puts b.go_to_brooklyn   #for my instance
-  #name the variable you created (b is the class) name the method (go_to_brooklyn)
+  # name the variable you created (b is the class) name the method (go_to_brooklyn)
   
 
 
